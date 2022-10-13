325f0cb243c565dbc54ef8a1bdeb18941c8a4b09
Pavel Biryukov <biryukovpyu@gmail.com>
2022-10-14 00:06:39 +0300

    test: add test for compare yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,8 +8,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('testing function genDiff', () => {
+test('testing function genDiff for json format', () => {
   const expected = readFile('expected_file.json');
   const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  expect(actual).toEqual(expected);
+});
+
+test('testing function genDiff for yaml format', () => {
+  const expected = readFile('expected_file.json');
+  const actual = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
   expect(actual).toEqual(expected);
 });
