caa19872a5bef3ff6950116e762ed0897d65a115
Pavel Biryukov <biryukovpyu@gmail.com>
2022-10-15 00:12:07 +0300

    feat: add recursive compare

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import process from 'process';
 import _ from 'lodash';
 import parse from './parsers.js';
+import stylish from './stylish.js';
 
 const getData = (filepath) => {
   const fullPath = path.resolve(process.cwd(), filepath);
@@ -16,34 +17,43 @@ const buildDiff = (data1, data2) => {
   const keys2 = Object.keys(data2);
   const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
-  const indent = '  ';
-  const lines = sortedKeys.reduce((acc, key) => {
-    let result;
+  return sortedKeys.map((key) => {
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return { key, children: buildDiff(data1[key], data2[key]), status: 'nested' };
+    }
     if (!Object.hasOwn(data1, key)) {
-      result = [...acc, `${indent}+ ${key}: ${data2[key]}`];
-    } else if (!Object.hasOwn(data2, key)) {
-      result = [...acc, `${indent}- ${key}: ${data1[key]}`];
-    } else if (data1[key] !== data2[key]) {
-      result = [
-        ...acc,
-        `${indent}- ${key}: ${data1[key]}`,
-        `${indent}+ ${key}: ${data2[key]}`,
-      ];
-    } else {
-      result = [...acc, `${indent}  ${key}: ${data1[key]}`];
+      return { key, value: data2[key], status: 'added' };
+    }
+    if (!Object.hasOwn(data2, key)) {
+      return { key, value: data1[key], status: 'deleted' };
+    }
+    if (data1[key] !== data2[key]) {
+      return {
+        key,
+        valueOld: data1[key],
+        valueNew: data2[key],
+        status: 'changed',
+      };
     }
 
-    return result;
-  }, []);
-
-  return ['{', ...lines, '}'].join('\n');
+    return { key, value: data1[key], status: 'unchanged' };
+  });
 };
 
-const genDiff = (filepath1, filepath2) => {
+const formatDiff = (data, format) => {
+  switch (format) {
+    case 'stylish':
+      return stylish(data);
+    default:
+      throw new Error(`Unknown formatter: '${format}'!`);
+  }
+};
+
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = parse(getData(filepath1), getExtension(filepath1));
   const data2 = parse(getData(filepath2), getExtension(filepath2));
 
-  return buildDiff(data1, data2);
+  return formatDiff(buildDiff(data1, data2), formatName);
 };
 
 export default genDiff;
