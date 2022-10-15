09c0476070040a75b2ec6814838aa9145f270963
Pavel Biryukov <biryukovpyu@gmail.com>
2022-10-15 18:08:02 +0300

    feat: add json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -7,6 +7,8 @@ const formatDiff = (data, format) => {
       return stylish(data);
     case 'plain':
       return plain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       throw new Error(`Unknown formatter: '${format}'!`);
   }
