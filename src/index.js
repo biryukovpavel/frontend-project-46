0971231f770a7841bcfbcaa434f11b9a9313371d
Pavel Biryukov <biryukovpyu@gmail.com>
2022-10-17 15:59:41 +0300

    refactor: extract buildDiff function into separate file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import _ from 'lodash';
 import parse from './parsers.js';
+import buildDiff from './buildDiff.js';
 import formatDiff from './formatters/index.js';
 
 const getData = (filepath) => {
@@ -11,34 +11,6 @@ const getData = (filepath) => {
 };
 
 const getExtension = (filepath) => path.extname(filepath).slice(1);
-
-const buildDiff = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const sortedKeys = _.sortBy(_.union(keys1, keys2));
-
-  return sortedKeys.map((key) => {
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return { key, children: buildDiff(data1[key], data2[key]), status: 'nested' };
-    }
-    if (!Object.hasOwn(data1, key)) {
-      return { key, value: data2[key], status: 'added' };
-    }
-    if (!Object.hasOwn(data2, key)) {
-      return { key, value: data1[key], status: 'deleted' };
-    }
-    if (data1[key] !== data2[key]) {
-      return {
-        key,
-        valueOld: data1[key],
-        valueNew: data2[key],
-        status: 'changed',
-      };
-    }
-
-    return { key, value: data1[key], status: 'unchanged' };
-  });
-};
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = parse(getData(filepath1), getExtension(filepath1));
